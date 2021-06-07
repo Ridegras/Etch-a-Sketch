3ce269c746b5188b576b0f4c0b1b47d814cd5cda
Ridegras <franciscominutti.fm@gmail.com>
2021-06-07 15:09:47 -0300

    -Proyecto basico terminado. -Proximas versiones tendran mejoras. -Funciones basicas adquiridas (color, clear y grid-lines).

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const body = document.querySelector('body');
 const start = document.querySelector('.start');
 const grid = document.createElement('div');
 grid.classList.add('grid');
+const lines = document.querySelector('#lines');
+const clear = document.querySelector('#clear');
 
 let measures = 256;
 
@@ -12,7 +14,7 @@ function randomNum(){
 const mesaurement = ()=>{
     for (let i = 0; i < measures; i++) {
         let divGrid = document.createElement('div');
-        divGrid.classList.add('div-grid');
+        divGrid.setAttribute('id', 'pixel');
 
         divGrid.onmouseover = ()=>{
             divGrid.style.cssText = `background-color: rgb(${randomNum()},${randomNum()},${randomNum()});`;
@@ -23,8 +25,31 @@ const mesaurement = ()=>{
 };
 
 //Aca empieza el juego
+
 start.addEventListener('click', ()=>{
     body.removeChild(start);
     body.appendChild(grid);
     mesaurement();
 });
+
+lines.addEventListener('click', ()=>{
+    let divGrid = document.querySelectorAll('#pixel');
+    divGrid.forEach(pixel => {
+        pixel.classList.toggle('div-grid');
+    });
+});
+
+clear.addEventListener('click', ()=>{
+    let altoXancho = parseInt(prompt('Alto y ancho de pixeles (ingresa el numero entero)'));
+    if (altoXancho > 0 && altoXancho < 51) {
+        grid.style.cssText = `grid-template-columns: repeat(${altoXancho}, 1fr); grid-template-rows: repeat(${altoXancho}, 1fr);`;
+        measures = altoXancho * altoXancho;
+        grid.innerHTML = '';
+        mesaurement();
+    }
+    else{
+        alert('Ingreso incorrecto. Vuelve a clickear "Clear" e ingresa un numero valido entre 1 y 50');
+    }
+    
+
+})
